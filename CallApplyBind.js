69a9e33048e14445e5b784105967ad305f780496
savanvekariya <savanvekariya.sv@gmail.com>
2023-12-05 15:18:52 +0100

    call

--- CallApplyBind.js
+++ CallApplyBind.js
@@ -1,44 +1,55 @@
 const person1 = {
-    name: "Bob",
-    age: 26,
-    showDetails: function (city) {
-        console.log(this.name + " " + city)
-    }
-}
+  name: "Bob",
+  age: 26,
+  showDetails: function (city) {
+    console.log(this.name + " " + city);
+  },
+};
 
 const person2 = {
-    name: "Martin",
-    age: 30
-}
+  name: "Martin",
+  age: 30,
+};
 
-person1.showDetails.call(person2, "Toronto")
-
+person1.showDetails.call(person2, "Toronto");
 
 // Diffrent way
 
 const student1 = {
-    name: "Tom",
-    age: 20
-}
+  name: "Tom",
+  age: 20,
+};
 
 const student2 = {
-    name: "Jerry",
-    age: 23
-}
+  name: "Jerry",
+  age: 23,
+};
 
 function studentDetails(city) {
-    console.log(this.name + " " + this.age + " " + city)
+  console.log(this.name + " " + this.age + " " + city);
 }
 
 //call - Invokes the function with provided this value and arguments one by one.
-studentDetails.call(student1, "Frankfurt")
-studentDetails.call(student2, "Berlin")
+studentDetails.call(student1, "Frankfurt");
+studentDetails.call(student2, "Berlin");
 
 //Apply - Invokes the function with given this value and arguments as array.
-studentDetails.apply(student1, ["Frankfurt"])
+studentDetails.apply(student1, ["Frankfurt"]);
 
 //Bind - Returns the function with the given this value and arguments.
-const resultFunc = studentDetails.bind(student1)
-resultFunc("Frankfurt") //Frankfurt
-const resultFunc2 = studentDetails.bind(student1, "Munich")
-resultFunc2("Frankfurt") //Munich
+const resultFunc = studentDetails.bind(student1);
+resultFunc("Frankfurt"); //Frankfurt
+const resultFunc2 = studentDetails.bind(student1, "Munich");
+resultFunc2("Frankfurt"); //Munich
+
+//call
+
+const person = {
+  name: "Bob",
+};
+
+function printDetails(city) {
+  console.log("My name is " + this.name + ". " + "I am living in " + city);
+}
+
+printDetails.call(person, "Koblenz");
