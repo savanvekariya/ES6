c505df1d06b790ae1e79afb9a624f08fc1315bf1
savanvekariya <savanvekariya.sv@gmail.com>
2023-12-05 15:21:14 +0100

    closure

--- closures.js
+++ closures.js
@@ -1,28 +1,35 @@
 //closure - Closure is the form of lexical scoping used to preserve the variables from outer scope of the function in inner scope of function
 //Lexical Scoping - It is the process used to define the scope of varible by its position in the sorce code.
 
-function closureFunc(a,b){
-    let c = 2
-    return function(d){
-        return a+b+c+d
-    }
+function closureFunc(a, b) {
+  let c = 2;
+  return function (d) {
+    return a + b + c + d;
+  };
 }
 
-let resultFunc = closureFunc(3,4)
-let sum = resultFunc(5)
+let resultFunc = closureFunc(3, 4);
+let sum = resultFunc(5);
 
-
-function SeconfClosureFunc(i){
-    return {
-        twoSum : function(a){
-            return a+i
-        },
-        threeSum : function(a,b){
-            return a+b+i
-        }
-    }
+function SeconfClosureFunc(i) {
+  return {
+    twoSum: function (a) {
+      return a + i;
+    },
+    threeSum: function (a, b) {
+      return a + b + i;
+    },
+  };
 }
 
-let SecondResultFunc = SeconfClosureFunc(1)
-let twoSumResult = SecondResultFunc.twoSum(2)
-let threeResult = SecondResultFunc.threeSum(3,4)
+let SecondResultFunc = SeconfClosureFunc(1);
+let twoSumResult = SecondResultFunc.twoSum(2);
+let threeResult = SecondResultFunc.threeSum(3, 4);
+
+function Parent(name) {
+  return () => {
+    console.log(name);
+  };
+}
+
+Parent("Uwe")();
